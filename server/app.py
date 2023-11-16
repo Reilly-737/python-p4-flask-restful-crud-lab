155af7514be1f708415ea723662bb579d5bc60ab
Reilly <reillyswork@gmail.com>
2023-11-16 12:12:37 -0700

    finished lab

--- server/app.py
+++ server/app.py
@@ -46,8 +46,32 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-
-
+    
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first()
+        
+        if plant: 
+            for attr in data: 
+                setattr(plant, attr, data[attr])
+            db.session.add(plant)
+            db.session.commit()
+            
+            return make_response(plant.to_dict(), 200)
+        else:
+            return make_response({"error": "Plant not found"}, 404)
+        
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            
+            return make_response('', 204)
+        else: 
+            return make_response({"error": "Plant not found"}, 404)
+            
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
